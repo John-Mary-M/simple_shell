6c3b909207aaccb3b43fc526392370771cc861c5
Abel Sekibaala <abel.sekibaala@gmail.com>
2023-05-23 10:33:21 -0700

    Entry to the programme

--- main.c
+++ main.c
@@ -1,31 +1,57 @@
 #include "main.h"
-
 /**
- * main - entry point for the whole shell.
- *
- * Return: 0 on sucess.
+ * free_data - frees data structure
+ * @datash: data structure
+ * Return: no return
  */
-int main(void)
+void free_data(data_shell *datash)
 {
-	char command[MAX_COMMAND_LENGTH] = "";
-
-	while (1)
-	{
-		write(1, "Cisfun$ ", 9);
-
-		if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
-		{
-			perror("fgets");
-			exit(1);
-		}
-
-		command[strcspn(command, "\n")] = '\0';
-
-		if (strlen(command) == 0)
-			continue;
-		if (strcmp(command, "exit") == 0)
-			break;
-		run_command_with_args(command);
-	}
-	return (0);
+unsigned int i;
+for (i = 0; datash->_environ[i]; i++)
+{
+free(datash->_environ[i]);
+}
+free(datash->_environ);
+free(datash->pid);
+}
+/**
+ * set_data - Initialize data structure
+ * @datash: data structure
+ * @av: argument vector
+*/
+void set_data(data_shell *datash, char **av)
+{
+unsigned int i;
+datash->av = av;
+datash->input = NULL;
+datash->args = NULL;
+datash->status = 0;
+datash->counter = 1;
+for (i = 0; environ[i]; i++)
+;
+datash->_environ = malloc(sizeof(char *) * (i + 1));
+for (i = 0; environ[i]; i++)
+{
+datash->_environ[i] = _strdup(environ[i]);
+}
+datash->_environ[i] = NULL;
+datash->pid = aux_itoa(getpid());
+}
+/**
+ * main - Entry point
+ * @ac: argument count
+ * @av: argument vector
+ * Return: 0 on success.
+ */
+int main(int ac, char **av)
+{
+data_shell datash;
+(void) ac;
+signal(SIGINT, get_sigint);
+set_data(&datash, av);
+shell_loop(&datash);
+free_data(&datash);
+if (datash.status < 0)
+return (255);
+return (datash.status);
 }
